c37dd8a6855dbb68972153626d780f565390fb2c
GiackAloZ <giacomo.aloisi1998@gmail.com>
2020-05-08 18:31:54 +0200

    Remove unused imports

--- src/main/kotlin/model/Constraint.kt
+++ src/main/kotlin/model/Constraint.kt
@@ -1,13 +1,6 @@
 package model
 
 import model.DiceConstraints.nullable
-import org.jetbrains.exposed.dao.IntEntity
-import org.jetbrains.exposed.dao.IntEntityClass
-import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.IdTable
-import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.Table
 
 abstract class Constraints(name: String) : AdventureTable(name) {
     val choice = reference("choice", Choices)
--- src/main/kotlin/model/Enemy.kt
+++ src/main/kotlin/model/Enemy.kt
@@ -1,11 +1,5 @@
 package model
 
-import org.jetbrains.exposed.dao.IntEntity
-import org.jetbrains.exposed.dao.IntEntityClass
-import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.IdTable
-import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
 object Enemies : AdventureTable("ENEMY") {
