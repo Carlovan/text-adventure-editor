4c0b9e3f41ee1257959ba583b83ace7962d0882b
Carlovan <giulio.carlassare@gmail.com>
2020-05-10 12:41:11 +0200

    Better handling of subview in MainView

--- src/main/kotlin/views/MainView.kt
+++ src/main/kotlin/views/MainView.kt
@@ -1,42 +1,27 @@
 package views
 
-import controller.AdventureController
-import javafx.collections.ObservableList
-import javafx.scene.control.ComboBox
 import tornadofx.*
-import viewmodel.AdventureViewModel
-import viewmodel.StepViewModel
-import java.util.*
-import kotlin.reflect.KClass
 
 class MainView: View() {
-    override val root = borderpane()
-
-    var contentView: View = find(StepsMasterView::class)
-
-    init {
-        with(root) {
-            top = menubar {
-                menu("Adventure") {
-                    item("Change adventure") {
-                        action {
-                            replaceWith<SelectAdventureView>()
-                        }
+    override val root = borderpane {
+        top = menubar {
+            menu("Adventure") {
+                item("Change adventure") {
+                    action {
+                        replaceWith<SelectAdventureView>()
                     }
-                    item("Steps") {
-                        action {
-                            contentView.replaceWith<StepsMasterView>()
-                            contentView = find(StepsMasterView::class)
-                        }
+                }
+                item("Steps") {
+                    action {
+                        center<StepsMasterView>()
                     }
                 }
             }
-
-            center<StepsMasterView>()
         }
     }
 
     override fun onDock() {
-        contentView.onDock()
+        root.center = null;
+        root.center<StepsMasterView>()
     }
 }
