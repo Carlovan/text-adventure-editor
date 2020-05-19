4de30b6be6b61bd17c6ccedd86c9a855a72490da
Carlovan <giulio.carlassare@gmail.com>
2020-05-19 14:56:59 +0200

    Set repetitionAttempts to 0 to correctly catch update errors

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 import model.*
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.ThreadLocalTransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 import tornadofx.launch
 
@@ -8,7 +9,7 @@ fun main(args: Array<String>) {
     Database.connect(url = "jdbc:postgresql://cauldron.liquoricemage.it:5432/textadveditor_db",
                      driver = "org.postgresql.Driver",
                      user = "textadveditor_u",
-                     password = "gaheANeHTMOYDd2tfko7HGmN")
+                     password = "gaheANeHTMOYDd2tfko7HGmN", manager = { ThreadLocalTransactionManager(it, 0) })
 
     transaction {
         addLogger(StdOutSqlLogger)
