55fd95831c30879b659a229b31112bddcc96552a
Carlovan <giulio.carlassare@gmail.com>
2020-05-23 18:30:14 +0200

    fixup! Added check on Step number

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,7 +1,11 @@
 import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 fun <T> Optional<T>.peek(op: (T) -> Unit) = this.apply { ifPresent(op) }
 fun <T> Optional<T>.onEmpty(op: () -> Unit) = this.apply { if (!isPresent) op() }
+
+val ExposedExecutor: ExecutorService = Executors.newSingleThreadExecutor()
 
 fun String.ellipses(maxLength: Int) =
     if (this.length > maxLength - 3) {
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,3 @@
-import kotlinx.coroutines.*
 import model.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
@@ -6,13 +5,9 @@ import org.jetbrains.exposed.sql.StdOutSqlLogger
 import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 import tornadofx.launch
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-
-val ExposedExecutor: ExecutorService = Executors.newSingleThreadExecutor()
 
 fun main(args: Array<String>) {
-    ExposedScope.launch {
+    ExposedExecutor.submit {
         Database.connect(
             url = "jdbc:postgresql://cauldron.liquoricemage.it:5432/textadveditor_db",
             driver = "org.postgresql.Driver",
@@ -23,6 +18,11 @@ fun main(args: Array<String>) {
 
         transaction {
             addLogger(StdOutSqlLogger)
+//            SchemaUtils.drop(Steps, Choices, DiceConstraints,
+//                Skills, PlayerConfigurations, SkillsPlayerConfigurations, SkillConstraints,
+//                Statistics, StatisticContraints, StatisticsSkills, Enemies, EnemiesStatistics,
+//                Items, ItemSlots, Loots, LootsItems, PlayerAvailableSlots, EnemiesSteps, StatisticsItems,
+//                ItemConstraints, ItemSkillActivations, StatisticsPlayerConfigurations)
 
             SchemaUtils.create(
                 Adventures, Steps, Choices, DiceConstraints,
