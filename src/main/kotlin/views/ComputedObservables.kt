193f5e1bd3113a88bf07c91a72ca9a7f3edcab47
Carlovan <giulio.carlassare@gmail.com>
2020-05-23 19:09:55 +0200

    Removed warnings

--- src/main/kotlin/views/ComputedObservables.kt
+++ src/main/kotlin/views/ComputedObservables.kt
@@ -10,44 +10,47 @@ import tornadofx.booleanProperty
 import tornadofx.editModel
 import tornadofx.nonNullObjectBinding
 
-fun <T> ObservableValue<out ObservableValue<T>>.flatten(): ObservableValue<T> = object: ObservableValueBase<T>() {
-        init {
-            this@flatten.addListener(this::externalUpdateHandler)
-            this@flatten.value?.addListener(this::internalUpdateHandler)
-        }
+fun <T> ObservableValue<out ObservableValue<T>>.flatten(): ObservableValue<T> = object : ObservableValueBase<T>() {
+    val handler: (ObservableValue<out T>, T, T) -> Unit = { _, _, _ -> this.internalUpdateHandler() }
 
-        private fun externalUpdateHandler(observable: ObservableValue<out ObservableValue<T>>?, oldValue: ObservableValue<T>, newValue: ObservableValue<T>) {
-            oldValue.removeListener(this::internalUpdateHandler)
-            newValue.addListener(this::internalUpdateHandler)
-            fireValueChangedEvent()
-        }
-
-        private fun internalUpdateHandler(observable: ObservableValue<out T>?, oldValue: T, newValue: T) {
-            fireValueChangedEvent()
-        }
-
-        override fun getValue() = this@flatten.value.value
+    init {
+        this@flatten.addListener { _, oldValue, newValue -> this.externalUpdateHandler(oldValue, newValue) }
+        this@flatten.value?.addListener(handler)
     }
+
+    private fun externalUpdateHandler(oldValue: ObservableValue<T>, newValue: ObservableValue<T>) {
+        oldValue.removeListener(handler)
+        newValue.addListener(handler)
+        fireValueChangedEvent()
+    }
+
+    private fun internalUpdateHandler() {
+        fireValueChangedEvent()
+    }
+
+    override fun getValue() = this@flatten.value.value
+}
 
 /**
  * Check if any item in the [TableView] is dirty
  */
-val <T> TableView<T>.isDirty: ObservableValue<Boolean> get() {
-    return nonNullObjectBinding(this, editModel.items) {
-        with(editModel.items.map { it.value.dirty }) {
-            if (this.isNotEmpty())
-                reduce { acc, value -> acc.or(value) }
-            else
-                 booleanProperty(false)
-        }
-    }.flatten()
-}
+val <T> TableView<T>.isDirty: ObservableValue<Boolean>
+    get() {
+        return nonNullObjectBinding(this, editModel.items) {
+            with(editModel.items.map { it.value.dirty }) {
+                if (this.isNotEmpty())
+                    reduce { acc, value -> acc.or(value) }
+                else
+                    booleanProperty(false)
+            }
+        }.flatten()
+    }
 
 val <T> TableView<T>.anySelected: ObservableBooleanValue
-get() = this.selectionModel.selectedItemProperty().isNotNull
+    get() = this.selectionModel.selectedItemProperty().isNotNull
 
 val <T> ComboBox<T>.anySelected: ObservableBooleanValue
-get() = this.selectionModel.selectedItemProperty().isNotNull
+    get() = this.selectionModel.selectedItemProperty().isNotNull
 
 val <T> ListView<T>.anySelected: ObservableBooleanValue
-get() = this.selectionModel.selectedItemProperty().isNotNull
+    get() = this.selectionModel.selectedItemProperty().isNotNull
