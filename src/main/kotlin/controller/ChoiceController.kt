07ad40c2930291fd1074c23e902eeeac0a33d908
GiackAloZ <giacomo.aloisi1998@gmail.com>
2020-06-05 23:00:48 +0200

    Add controller create and delete constraints

--- src/main/kotlin/controller/ChoiceController.kt
+++ src/main/kotlin/controller/ChoiceController.kt
@@ -1,10 +1,9 @@
 package controller
 
-import model.Choice
+import model.*
+import sqlutils.MaybePSQLError
 import sqlutils.safeTransaction
-import viewmodel.ChoiceViewModel
-import viewmodel.DetailStepViewModel
-import viewmodel.fromViewModel
+import viewmodel.*
 
 class ChoiceController: ControllerWithContextAdventure() {
     fun createChoice(choice: ChoiceViewModel, fromStep: DetailStepViewModel) =
@@ -20,4 +19,52 @@ class ChoiceController: ControllerWithContextAdventure() {
         safeTransaction {
             choice.item.delete()
         }
+
+    fun addConstraint(choice: ChoiceViewModel, constraint: ConstraintViewModel) : MaybePSQLError{
+        return safeTransaction { when(constraint.type) {
+            ConstraintType.DICE -> addDiceConstraint(choice, constraint as DiceConstraintViewModel)
+            ConstraintType.SKILL -> addSkillConstraint(choice, constraint as SkillConstraintViewModel)
+            ConstraintType.STATISTIC -> addStatisticConstraint(choice, constraint as StatisticConstraintViewModel)
+            ConstraintType.ITEM -> addItemConstraint(choice, constraint as ItemConstraintViewModel)
+        } }
+    }
+
+    private fun addDiceConstraint(choice: ChoiceViewModel, constraint: DiceConstraintViewModel) {
+        DiceConstraint.new {
+            adventure = contextAdventure!!.item.id
+            this.choice = choice.item.id
+            minValue = constraint.minValue.value
+            maxValue = constraint.maxValue.value
+        }
+    }
+
+    private fun addSkillConstraint(choice: ChoiceViewModel, constraint: SkillConstraintViewModel) {
+        SkillConstraint.new {
+            adventure = contextAdventure!!.item.id
+            this.choice = choice.item.id
+            skill = constraint.skillViewModel.value.item
+        }
+    }
+
+    private fun addStatisticConstraint(choice: ChoiceViewModel, constraint: StatisticConstraintViewModel) {
+        StatisticConstraint.new {
+            adventure = contextAdventure!!.item.id
+            this.choice = choice.item.id
+            statistic = constraint.statViewModel.value.item
+            minValue = constraint.minValue.value
+            maxValue = constraint.maxValue.value
+        }
+    }
+
+    private fun addItemConstraint(choice: ChoiceViewModel, constraint: ItemConstraintViewModel) {
+        ItemConstraint.new {
+            adventure = contextAdventure!!.item.id
+            this.choice = choice.item.id
+            item = constraint.itemViewModel.value.item
+            quantity = constraint.quantity.value
+            isConsumed = constraint.isConsumed.value
+        }
+    }
+
+    fun removeConstraint(constraint: ConstraintViewModel) = safeTransaction { constraint.innerItem.item.delete() }
 }
